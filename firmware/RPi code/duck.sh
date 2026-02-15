6f7f9b0d971522468100b79a5106d77723833863
z0b1 <yu4tmb@gmail.com>
2026-02-16 00:20:08 +0100

    removed duck.sh as i will be using zrok.io

--- firmware/RPi code/duck.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# DuckDNS Update Script for z0b1 Weather Station
-# Replace YOUR_DOMAIN and YOUR_TOKEN with your actual details from duckdns.org
-
-DOMAIN="YOUR_DOMAIN"
-TOKEN="YOUR_TOKEN"
-
-echo url="https://www.duckdns.org/update?domains=$DOMAIN&token=$TOKEN&ip=" | curl -k -o ~/duckdns/duck.log -K -
